f84892db654076b602d31404197aaaa4845e49f8
Tommy Berman <tomy.berman1@gmail.com>
2021-06-04 14:13:16 +0000

    Automatically backed up by Learn

---
+++ arguments.rb
@@ -0,0 +1,11 @@
+def greeting_a_person(name)
+  puts "Hello #{name}"
+end
+
+def greeting_programmer(name, language = "computer")
+  puts "Hello, #{name}. We heard you are a great #{language} programmer."
+end
+
+greeting_programmer("Steven", "Ruby")
+
+greeting_programmer("Maria")
---
+++ learning_while.rb
@@ -0,0 +1,22 @@
+while nil do
+  puts "I will never run"
+end
+
+#count = 0 
+#while count < 3 do
+#  puts "I am the #{count}, I love to count!"
+#  count += 1
+#end
+
+#magic_exit_number = 7
+#count = 0 
+#while count < 10 do
+#  break if count == magic_exit_number
+#  puts "I am the #{count}, I love to count!"
+#  count += 1 
+#end
+
+
+7.times do
+  puts "I love u Dana <3"
+end
